f8e017b67fea640f1037ebca7cf94cf89d1db64f
YangRui2015 <yangrui19@mails.tsinghua.edu.cn>
2024-10-17 05:48:54 +0800

    first update

---
+++ README.md
@@ -0,0 +1,22 @@
+# Generalizable-Reward-Model
+Code for NeurIPS 2024 paper "Regularizing Hidden States Enables Learning Generalizable Reward Model for LLMs". 
+
+
+
+
+
+
+
+
+
+
+## Citation
+
+``
+@article{yang2024regularizing,
+  title={Regularizing Hidden States Enables Learning Generalizable Reward Model for LLMs},
+  author={Yang, Rui and Ding, Ruomeng and Lin, Yong and Zhang, Huan and Zhang, Tong},
+  journal={arXiv preprint arXiv:2406.10216},
+  year={2024}
+}
+``
---
+++ reward_models/load_datasets.py
@@ -0,0 +1,182 @@
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+from datasets import load_dataset, concatenate_datasets
+
+# for vanilla chosen and reject style dataset, such as dendrydong/preference_700K
+def build_dataset(data_path, tokenizer, split='train', size=None, model_name=''):
+    ds = load_dataset(data_path, split=split)
+    
+    if size is not None:
+        ds = ds.select(range(0, size))
+
+    def formatting_func(example):
+        kwargs = {"padding": True, "truncation": True, "max_length": tokenizer.max_length, "return_tensors": "pt"}
+        chosen_messages = example['chosen']
+        rejected_messages = example['rejected']
+        prompt_plus_chosen_response = tokenizer.apply_chat_template(chosen_messages, tokenize=False)
+        prompt_plus_rejected_response = tokenizer.apply_chat_template(rejected_messages, tokenize=False)
+        tokens_chosen = tokenizer.encode_plus(prompt_plus_chosen_response, **kwargs)
+        tokens_rejected = tokenizer.encode_plus(prompt_plus_rejected_response, **kwargs)
+
+        if 'GRM' in model_name:
+            # add label mask for sft and dpo training
+            prompt = example['chosen'][:-1]
+            prompt_template = tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)
+            tokens_prompt = tokenizer.encode_plus(prompt_template, **kwargs)['input_ids'][0]
+            label_chosen = tokens_chosen["input_ids"][0].clone()
+            label_chosen[:len(tokens_prompt)] = -100
+            label_rejected = tokens_rejected["input_ids"][0].clone()
+            label_rejected[:len(tokens_prompt)] = -100
+            return {
+                "input_ids_chosen": tokens_chosen["input_ids"][0], "attention_mask_chosen": tokens_chosen["attention_mask"][0],
+                "input_ids_rejected": tokens_rejected["input_ids"][0], "attention_mask_rejected": tokens_rejected["attention_mask"][0],
+                "label_chosen": label_chosen,  'label_rejected': label_rejected
+            }
+        else:
+            return {
+                "input_ids_chosen": tokens_chosen["input_ids"][0], "attention_mask_chosen": tokens_chosen["attention_mask"][0],
+                "input_ids_rejected": tokens_rejected["input_ids"][0], "attention_mask_rejected": tokens_rejected["attention_mask"][0],
+            }
+
+    ds = ds.map(formatting_func, batched=False, num_proc=10) 
+    remove_columns = []
+    for col in ds.column_names:
+        if 'input' not in col and 'attention' not in col and 'label' not in col:
+            remove_columns.append(col)
+    ds = ds.remove_columns(remove_columns)
+
+    ds.set_format(type="torch")
+    return ds
+
+
+# for UnifiedFeedback
+def build_dataset_UF(data_path, tokenizer, split='train', size=None, mode='', model_name=''):
+    try:
+        ds = load_dataset(data_path, 'all', split=split)
+    except:
+        ds = load_dataset(data_path, split=split)
+    
+    # filter data with the same rating
+    ds = ds.filter(lambda example: example['conv_A_rating'] != example['conv_B_rating'], num_proc=30)
+
+    if len(mode):
+        if mode == '40k':
+            ds = ds.select(range(0, len(ds), 20)) 
+        elif mode == '400k':
+            ds = ds.select(range(0, len(ds), 2)) 
+
+    if size is not None:
+        ds = ds.select(range(0, size))
+
+    def formatting_func(example):
+        kwargs = {"padding": True, "truncation": True, "max_length": tokenizer.max_length, "return_tensors": "pt"}
+        if example['conv_A_rating'] > example['conv_B_rating']:
+            chosen_messages = example['conv_A']
+            rejected_messages = example['conv_B']
+            margin = example['conv_A_rating'] - example['conv_B_rating']
+        else:
+            chosen_messages = example['conv_B']
+            rejected_messages = example['conv_A']
+            margin = example['conv_B_rating'] - example['conv_A_rating']
+        
+        if 'summarize' in example['source']:
+            chosen_messages[0]['content'] = 'Generate one-sentence summary for the following post: ' + chosen_messages[0]['content'].strip()
+            rejected_messages[0]['content'] = 'Generate one-sentence summary for the following post: ' + rejected_messages[0]['content'].strip()
+        
+        prompt_plus_chosen_response = tokenizer.apply_chat_template(chosen_messages, tokenize=False)
+        prompt_plus_rejected_response = tokenizer.apply_chat_template(rejected_messages, tokenize=False)
+        tokens_chosen = tokenizer.encode_plus(prompt_plus_chosen_response, **kwargs)
+        tokens_rejected = tokenizer.encode_plus(prompt_plus_rejected_response, **kwargs)
+        if 'GRM' in model_name:
+            # add label mask for sft and dpo training
+            prompt = [example['conv_A'][0]]
+            prompt_template = tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)
+            tokens_prompt = tokenizer.encode_plus(prompt_template, **kwargs)['input_ids'][0]
+            label_chosen = tokens_chosen["input_ids"][0].clone()
+            label_chosen[:len(tokens_prompt)] = -100
+            label_rejected = tokens_rejected["input_ids"][0].clone()
+            label_rejected[:len(tokens_prompt)] = -100
+            return {
+                "input_ids_chosen": tokens_chosen["input_ids"][0], "attention_mask_chosen": tokens_chosen["attention_mask"][0],
+                "input_ids_rejected": tokens_rejected["input_ids"][0], "attention_mask_rejected": tokens_rejected["attention_mask"][0],
+                "label_chosen": label_chosen,  'label_rejected': label_rejected,
+                # "margin": margin, # GRM does not need this
+            }
+        else:
+            return {
+                "input_ids_chosen": tokens_chosen["input_ids"][0], "attention_mask_chosen": tokens_chosen["attention_mask"][0],
+                "input_ids_rejected": tokens_rejected["input_ids"][0], "attention_mask_rejected": tokens_rejected["attention_mask"][0],
+                "margin": margin, 
+            }
+        
+
+    ds = ds.map(formatting_func, batched=False, num_proc=10)
+    # ds = ds.filter(lambda x: len(x["input_ids_chosen"]) <= script_args.max_length and len(x["input_ids_rejected"]) <= script_args.max_length, num_proc=30)
+    remove_columns = []
+    for col in ds.column_names:
+        if 'input' not in col and 'attention' not in col and 'margin' not in col and 'label' not in col:
+            remove_columns.append(col)
+    ds = ds.remove_columns(remove_columns)
+
+    ds.set_format(type="torch")
+    return ds
+
+
+# for Skywork Reward Preference 80K
+def build_dataset_SK(data_path, tokenizer, split='train', size=None, model_name=''):
+    ds = load_dataset(data_path, split=split)
+
+    if size is not None:
+        ds = ds.select(range(0, size))
+
+    def formatting_func(example):
+        kwargs = {"padding": True, "truncation": True, "max_length": tokenizer.max_length, "return_tensors": "pt"}
+        prompt = example['chosen'][0]['content']
+
+        chosen_messages = example['chosen']
+        rejected_messages = example['rejected']
+
+        prompt_plus_chosen_response = tokenizer.apply_chat_template(chosen_messages, tokenize=False)
+        prompt_plus_rejected_response = tokenizer.apply_chat_template(rejected_messages, tokenize=False)
+        tokens_chosen = tokenizer.encode_plus(prompt_plus_chosen_response, **kwargs)
+        tokens_rejected = tokenizer.encode_plus(prompt_plus_rejected_response, **kwargs)
+        if 'GRM' in model_name:
+            # add label mask for sft and dpo
+            prompt_template = tokenizer.apply_chat_template([{"content": prompt, "role": "user" }], tokenize=False, add_generation_prompt=True)
+            tokens_prompt = tokenizer.encode_plus(prompt_template, **kwargs)['input_ids'][0]
+            label_chosen = tokens_chosen["input_ids"][0].clone()
+            label_chosen[:len(tokens_prompt)] = -100
+            label_rejected = tokens_rejected["input_ids"][0].clone()
+            label_rejected[:len(tokens_prompt)] = -100
+            return {
+                "input_ids_chosen": tokens_chosen["input_ids"][0], "attention_mask_chosen": tokens_chosen["attention_mask"][0],
+                "input_ids_rejected": tokens_rejected["input_ids"][0], "attention_mask_rejected": tokens_rejected["attention_mask"][0],
+                "label_chosen": label_chosen,  'label_rejected': label_rejected
+            }
+        else:
+            return {
+                "input_ids_chosen": tokens_chosen["input_ids"][0], "attention_mask_chosen": tokens_chosen["attention_mask"][0],
+                "input_ids_rejected": tokens_rejected["input_ids"][0], "attention_mask_rejected": tokens_rejected["attention_mask"][0],
+            }
+
+    ds = ds.map(formatting_func, batched=False, num_proc=10) 
+    ds.set_format(type="torch")
+    return ds
+
+
+def load_train_eval_dataset(data_path, tokenizer, size=None, mode='', model_name=''):
+    if 'Unified' in data_path:
+        # mode is only used for loading training data
+        train_dataset = build_dataset_UF(data_path, tokenizer, split='train', mode=mode, model_name=model_name) 
+        eval_dataset = build_dataset_UF(data_path, tokenizer, split='val', model_name=model_name)
+    elif 'Skywork' in data_path:
+        dataset = build_dataset_SK(data_path, tokenizer, split='train', model_name=model_name)
+        dataset_split = dataset.train_test_split(test_size=0.005)
+        train_dataset, eval_dataset = dataset_split['train'], dataset_split['test']
+    else:
+        dataset = build_dataset(data_path, tokenizer, split='train', model_name=model_name) 
+        dataset_split = dataset.train_test_split(test_size=0.01)
+        train_dataset, eval_dataset = dataset_split['train'], dataset_split['test']
+    return train_dataset, eval_dataset
