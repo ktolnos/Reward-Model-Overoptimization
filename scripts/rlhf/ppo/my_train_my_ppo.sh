f602cd8c0456f04e780491b0b673994a216cb158
eop <eop@cs.toronto.edu>
2025-05-21 14:42:40 -0400

    increased local_rollout_forward_batch_size

--- scripts/rlhf/ppo/my_train_my_ppo.sh
+++ scripts/rlhf/ppo/my_train_my_ppo.sh
@@ -24,7 +24,7 @@ CUDA_VISIBLE_DEVICES=${gpu}  accelerate launch  \
     --model_name_or_path ${base_model_name} \
     --sft_model_path ${base_model_name} \
     --reward_model_path ${reward_base_model} \
-    --local_rollout_forward_batch_size 8 \
+    --local_rollout_forward_batch_size 16 \
     --missing_eos_penalty 1.0 \
     --whiten_rewards True \
     --save_steps 0.025 \
