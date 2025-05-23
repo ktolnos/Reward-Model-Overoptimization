aa02a27386da6e52532ef60a839fb9b6ef446373
eop <eop@cs.toronto.edu>
2025-05-23 16:56:06 -0400

    LoRa, value from sft

--- scripts/rlhf/ppo/my_train_my_ppo.sh
+++ scripts/rlhf/ppo/my_train_my_ppo.sh
@@ -36,15 +36,7 @@ CUDA_VISIBLE_DEVICES=${gpu}  accelerate launch  \
     --use_peft True \
     --lora_r 32 \
     --lora_alpha 64 \
-    --lora_target_modules [
-        "q_proj",
-        "k_proj",
-        "v_proj",
-        "o_proj",
-        "gate_proj",
-        "up_proj",
-        "down_proj",
-    ],  # Target modules for LoRA \
+    --lora_target_modules="['q_proj', 'k_proj', 'v_proj', 'o_proj']" \
 #    --resume_from_checkpoint True \
 
     
