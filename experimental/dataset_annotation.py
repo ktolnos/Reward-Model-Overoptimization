ded6964c2cbdb130f9a84fd448cbb5b10c3d8195
eop <eop@cs.toronto.edu>
2025-06-04 18:53:20 -0400

    annotate with QRM

--- experimental/dataset_annotation.py
+++ experimental/dataset_annotation.py
@@ -99,6 +99,7 @@ def evaluate_with_reward_model(dataset, model, tokenizer, batch_size=8, max_leng
         torch.cuda.empty_cache()
         with torch.no_grad():
             outputs = model(**inputs)
+            print(outputs)
             all_rewards = outputs.logits.squeeze(-1).cpu().numpy()
 
         # Process the results
