7aaed31267cf238d3b568922321d0b49770892d0
eop <eop@cs.toronto.edu>
2025-06-10 18:04:44 -0400

    Bash for sbatch annotate_dataset.sh

---
+++ experimental/annotate_dataset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+##SBATCH --job-name=annotate_dataset
+##SBATCH --cpus-per-task=16
+##SBATCH --mem=16gb
+##SBATCH --gres=gpu:A100-PCI-80GB:1
+##SBATCH --time=24:00:00
+
+cd /nas/ucb/eop/Reward-Model-Overoptimization/experimental/
+export HF_HOME="/nas/ucb/eop/cache"
+
+srun python dataset_annotation.py
