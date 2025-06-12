838568751adea0b992a7a569aacbb20826745b56
eop <eop@cs.toronto.edu>
2025-06-12 16:14:21 -0400

    GRPO with reasoning reward models

--- reward_utils.py
+++ reward_utils.py
@@ -339,3 +339,23 @@ def extract_reward_from_response(response):
         return 1
     else:
         return -1
+
+
+def prepare_input(data: Union[torch.Tensor, Any], device) -> Union[torch.Tensor, Any]:
+        """
+        Prepares one `data` before feeding it to the model, be it a tensor or a nested list/dictionary of tensors.
+        Mainly moves tensors to the correct device and dtype.
+        """
+        if isinstance(data, Mapping):
+            return type(data)({k: prepare_input(v) for k, v in data.items()})
+        elif isinstance(data, (tuple, list)):
+            return type(data)(prepare_input(v) for v in data)
+        elif isinstance(data, torch.Tensor):
+            kwargs = {"device": device}
+            # if trainer.is_deepspeed_enabled and (torch.is_floating_point(data) or torch.is_complex(data)):
+            #     # NLP models inputs are int/uint and those get adjusted to the right dtype of the
+            #     # embedding. Other models such as wav2vec2's inputs are already float and thus
+            #     # may need special handling to match the dtypes of the model
+            #     kwargs.update({"dtype": torch.accelerator.state.deepspeed_plugin.hf_ds_config.dtype()})
+            return data.to(**kwargs)
+        return data
