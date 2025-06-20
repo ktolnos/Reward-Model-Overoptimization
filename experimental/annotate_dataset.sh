730e3f4896badd81fe01eefe96f3bbc8b5a03387
eop <eop@cs.toronto.edu>
2025-06-20 12:27:41 +0300

    Reference reward annotation

--- experimental/annotate_dataset.sh
+++ experimental/annotate_dataset.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=annotate_dataset
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=16gb
-#SBATCH --gres=gpu:A100-PCI-80GB:1
+#SBATCH --gres=gpu:A100-SXM4-80GB:1
 #SBATCH --time=4:00:00
 #SBATCH --qos=high
 
