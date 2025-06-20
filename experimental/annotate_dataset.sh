9330d29ce15d7b61f7920cf99a7ab07b1ef881b1
eop <eop@cs.toronto.edu>
2025-06-20 12:26:27 +0300

    Reference reward annotation

--- experimental/annotate_dataset.sh
+++ experimental/annotate_dataset.sh
@@ -5,6 +5,7 @@
 #SBATCH --mem=16gb
 #SBATCH --gres=gpu:A100-PCI-80GB:1
 #SBATCH --time=4:00:00
+#SBATCH --qos=high
 
 cd /nas/ucb/eop/Reward-Model-Overoptimization/experimental/
 export HF_HOME="/nas/ucb/eop/cache"
