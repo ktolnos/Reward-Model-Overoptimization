2eb1b66e05a2fad24cad6078ff70047f7ae363a2
eop <eop@cs.toronto.edu>
2025-07-04 22:04:32 +0300

    RM training with seeds, adv-rm impl

--- scripts/my_train_bt_rm_full.sh
+++ scripts/my_train_bt_rm_full.sh
@@ -18,12 +18,12 @@ wandb_name="${seed}_BT_RM_Qwen3-0.6B_${SLURM_JOB_ID}"
 log_dir='../save_reward_models'
 main_process_port=9994
 
-learning_rate=1e-5
+learning_rate=5e-6
 max_length=3000
 num_train_epochs=1
-gradient_accumulation_steps=8
-per_device_train_batch_size=8
-per_device_eval_batch_size=8
+gradient_accumulation_steps=16
+per_device_train_batch_size=4
+per_device_eval_batch_size=4
 
 cd ../reward_models
 CUDA_VISIBLE_DEVICES=${devices} accelerate launch --num_processes ${n_gpu} --main_process_port ${main_process_port} run_reward_models_train.py \
