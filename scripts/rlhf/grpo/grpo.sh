dd24b8c1347990342744a8f12836c671cb224629
eop <eop@cs.toronto.edu>
2025-08-20 13:42:09 +0300

    multi-gpu deepspeed

--- scripts/rlhf/grpo/grpo.sh
+++ scripts/rlhf/grpo/grpo.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=train_grpo
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32gb
-#SBATCH --gres=gpu:A100-SXM4-80GB:2
+#SBATCH --gres=gpu:A100-SXM4-80GB:1
 #SBATCH --time=24:00:00
 #SBATCH --qos=default
 
