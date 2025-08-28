4924bd140ff082daa73c7ed3943cac6776b3e609
eop <eop@cs.toronto.edu>
2025-08-28 17:06:55 +0300

    relu_chosen

--- scripts/rlhf/grpo/grpo.sh
+++ scripts/rlhf/grpo/grpo.sh
@@ -27,8 +27,8 @@ export WANDB_ARTIFACT_DIR="/nas/ucb/eop/cache/wandb-artifacts"
 log_dir="/nas/ucb/eop/Reward-Model-Overoptimization/scripts/rlhf/logs_grpo/$(date +%Y%m%d_%H%M%S)"
 #base_model_name="Qwen/Qwen3-0.6B" # policy base model
 base_model_name="Qwen/Qwen3-0.6B-Base" # policy base model
-#dataset_path="gagan3012/helpsteer2-preference-v2"
-dataset_path="/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/helpsteer_anntoated_policy_Qwen3-06B-Base_reward_Qwen3-0.6B_BT_RM_Qwen3-0.6B_len3000_fulltrain_1e-05"
+dataset_path="gagan3012/helpsteer2-preference-v2"
+#dataset_path="/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/helpsteer_anntoated_policy_Qwen3-06B-Base_reward_Qwen3-0.6B_BT_RM_Qwen3-0.6B_len3000_fulltrain_1e-05"
 #dataset_path="/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/helpsteer_anntoated_policy_Qwen3-06B_reward_Qwen-Embedding-8B-42"
 #dataset_path="/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/annotated_helpsteer2_Qwen06B-Base_policy_Qwen3-0.6B_42_BT_RM_Qwen3-0.6B_912840_len3000_fulltrain_4e-05_datahelpsteer2-preference-v2_reference"
 #dataset_path="/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/helpsteer_anntoated_policy_Qwen3_06B_reward_Gemma2_2B_ray_gold_URM_LLama8B/"
@@ -114,7 +114,7 @@ CUDA_VISIBLE_DEVICES=${gpu}  accelerate launch  \
     --save_generations_path "${log_dir}/generations.csv" \
     --adv_rm_lambda 0.0 \
     --online_pet_enabled True \
-    --preference_dataset_path "/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/helpsteer_anntoated_policy_Qwen3-06B-Base_reward_Qwen3-0.6B_BT_RM_Qwen3-0.6B_len3000_fulltrain_1e-05" \
+    --preference_dataset_path ${dataset_path} \
     --rm_gradient_checkpointing True \
     --move_rm_to_cpu True \
     --move_policy_to_cpu True \
