beb1741647290e6fd377426b2ea2e0a2b2251a37
eop <eop@cs.toronto.edu>
2025-10-22 16:54:26 -0400

    helpsteer3 gold-annotated rm grpo

--- evaluate_policy.sh
+++ evaluate_policy.sh
@@ -19,17 +19,17 @@ if [[ -v CHECKPOINTS_DIR_OVERRIDE ]]; then
 fi
 
 # Path to the training reward model
-TRAINING_RM_PATH="/nas/ucb/eop/Reward-Model-Overoptimization/save_reward_models/Qwen3-Embedding-8B_42_BT_RM_Qwen3-Embedding-8B_915487_len2000_fulltrain_2e-05_datahelpsteer2-preference-v2/logs/checkpoint-272"
+TRAINING_RM_PATH="/nas/ucb/eop/Reward-Model-Overoptimization/save_reward_models/Qwen3-0.6B_42_BT_RM_Qwen/Qwen3-0.6B_974219_len2000_fulltrain_2e-05_datahelpsteer3_goldSkywork-Reward-V2-Llama-3.1-8B-10k/logs/checkpoint-142"
 #TRAINING_RM_PATH="Ray2333/GRM-Gemma2-2B-rewardmodel-ft"
 
 # Name of the gold reward model
 #GOLD_RM_NAME="Ray2333/GRM-Gemma2-2B-rewardmodel-ft"
 #GOLD_RM_NAME="LxzGordon/URM-LLaMa-3.1-8B"
-GOLD_RM_NAME="nicolinho/QRM-Gemma-2-27B"
+GOLD_RM_NAME="Skywork/Skywork-Reward-V2-Llama-3.1-8B"
 
 # Dataset name
 #DATASET_NAME="/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/helpsteer2_gold_URM-LLaMa-3.1-8B_0_7951/"
-DATASET_NAME="ktolnos/helpsteer3-preference-chosenrrejected"
+DATASET_NAME="ktolnos/helpsteer3_goldSkywork-Reward-V2-Llama-3.1-8B-10k"
 
 
 # Base model name (required for LoRA checkpoints)
@@ -69,14 +69,15 @@ python evaluate_policy.py \
     --wandb_project "$WANDB_PROJECT" \
     --wandb_run_name "$WANDB_RUN_NAME" \
     --evaluate_with_training_rm True \
-    --evaluate_with_llm_judge True \
+    --evaluate_with_llm_judge False \
     --llm_judge_model_name "tngtech/deepseek-r1t2-chimera:free" \
     --baseline_model_path "Qwen/Qwen3-0.6B" \
-    --use_dataset_response_as_baseline True \
+    --use_dataset_response_as_baseline False \
     --save_eval_dataset_path "evaluation_dataset_${CHECKPOINTS_DIR##*/}_$(date +%Y%m%d_%H%M%S).json" \
-    --subsample_n 25 \
     ${DEBUG_MODE:-} \
     $([ ! -z "${BASE_MODEL_NAME:-}" ] && echo "--base_model_name $BASE_MODEL_NAME") \
+
+#     --subsample_n 25 \
 
 # To disable wandb logging, add: --disable_wandb
 # To enable debug mode, uncomment the DEBUG_MODE line above
