e1ee61067bff925e4368f7b7430f2836ee1b26b3
eop <eop@cs.toronto.edu>
2025-10-22 17:37:33 -0400

    helpsteer3 gold-annotated rm 6

--- scripts/my_train_bt_rm_full.sh
+++ scripts/my_train_bt_rm_full.sh
@@ -36,10 +36,10 @@ dataset_name=(
 #  "/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/Qwen3-8B-Embedding-Adv-RM-step_3"
 )
 base_model='Qwen/Qwen3-0.6B'
-seed=47
+seed=48
 wandb_name="${seed}_BT_RM_${base_model}_${SLURM_JOB_ID}"
 log_dir='../save_reward_models'
-main_process_port=9979
+main_process_port=9980
 
 learning_rate=2e-5
 max_length=2000
