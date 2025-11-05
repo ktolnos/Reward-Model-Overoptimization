43c15b96704c790c280d76f356b7ee06bd1a61bb
eop <eop@cs.toronto.edu>
2025-11-05 11:42:50 -0500

    helpsteer3 gold-annotated eval 10ep with other gold

--- evaluate_policy.sh
+++ evaluate_policy.sh
@@ -14,7 +14,7 @@ cd /nas/ucb/eop/Reward-Model-Overoptimization
 source .bashrc
 
 # Directory containing the checkpoints
-CHECKPOINTS_DIR="/nas/ucb/eop/Reward-Model-Overoptimization/scripts/rlhf/logs_grpo/20251024_174155"
+CHECKPOINTS_DIR="/nas/ucb/eop/Reward-Model-Overoptimization/scripts/rlhf/logs_grpo/20251028_180855"
 if [[ -v CHECKPOINTS_DIR_OVERRIDE ]]; then
   CHECKPOINTS_DIR="${CHECKPOINTS_DIR_OVERRIDE}"
 fi
