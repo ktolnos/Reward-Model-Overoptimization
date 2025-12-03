a18f9cba39e5dd7f4dc17ee80e4a90274835e344
eop <eop@cs.toronto.edu>
2025-12-03 12:28:39 -0500

    train 2ep rm

--- scripts/my_train_bt_rm_full.sh
+++ scripts/my_train_bt_rm_full.sh
@@ -29,28 +29,52 @@ n_gpu=1
 # dataset_name='hendrydong/preference_700K'
 #dataset_name='../experimental/data/helpsteer2_gold/'
 dataset_name=(
-   'ktolnos/helpsteer3_goldSkywork-Reward-V2-Llama-3.1-8B'
-#   'ktolnos/helpsteer3_goldSkywork-Reward-V2-Llama-3.1-8B-10k'
+   # 'ktolnos/helpsteer3_goldSkywork-Reward-V2-Llama-3.1-8B'
+  'ktolnos/helpsteer3_goldSkywork-Reward-V2-Llama-3.1-8B-10k'
 #  'gagan3012/helpsteer2-preference-v2'
 #  "/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/Qwen3-8B-Embedding-Adv-RM-step_1"
 #  "/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/Qwen3-8B-Embedding-Adv-RM-step_2"
 #  "/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/Qwen3-8B-Embedding-Adv-RM-step_3"
 )
 base_model='Qwen/Qwen3-0.6B'
-seed=42
-wandb_name="${seed}_BT_RM_${base_model}_${SLURM_JOB_ID}_helpsteer3_gold_full"
+seed=10
+wandb_name="${seed}_BT_RM_${base_model}_${SLURM_JOB_ID}_helpsteer3_gold_10k"
 log_dir='../save_reward_models'
-main_process_port=9982
+
+MIN_PORT=9900
+MAX_PORT=9999
+
+is_port_in_use() {
+    ss -lntu | grep -q ":$1 "
+}
+
+IDEAL_PORT=$((MIN_PORT + (SLURM_JOB_ID % (MAX_PORT - MIN_PORT + 1))))
+
+if ! is_port_in_use "${IDEAL_PORT}"; then
+    export MASTER_PORT=${IDEAL_PORT}
+else
+    for port in $(seq ${MIN_PORT} ${MAX_PORT}); do
+        if ! is_port_in_use "${port}"; then
+            export MASTER_PORT=${port}
+            break
+        fi
+    done
+fi
+
+if [[ -z "${MASTER_PORT}" ]]; then
+    echo "ERROR: Could not find any free port in the range ${MIN_PORT}-${MAX_PORT}." >&2
+    exit 1
+fi
 
 learning_rate=2e-5
 max_length=2000
-num_train_epochs=1
+num_train_epochs=2
 gradient_accumulation_steps=16
 per_device_train_batch_size=4
 per_device_eval_batch_size=4
 
 cd ../reward_models
-CUDA_VISIBLE_DEVICES=${devices} accelerate launch --num_processes ${n_gpu} --main_process_port ${main_process_port} run_reward_models_train.py \
+CUDA_VISIBLE_DEVICES=${devices} accelerate launch --num_processes ${n_gpu} --main_process_port ${MASTER_PORT} run_reward_models_train.py \
     --base_model ${base_model}  --wandb_name ${wandb_name}   --log_dir ${log_dir} \
     --num_train_epochs ${num_train_epochs} \
     --max_length ${max_length} \
@@ -62,4 +86,5 @@ CUDA_VISIBLE_DEVICES=${devices} accelerate launch --num_processes ${n_gpu} --mai
     --lr_scheduler_type "constant" \
     --dataset "${dataset_name[@]}" \
     --gradient_checkpointing False \
+    --warmup_ratio 0 \
     --seed ${seed} \
