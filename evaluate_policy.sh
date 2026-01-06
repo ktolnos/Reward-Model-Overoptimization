9c7200bf2278b3effb6e72c97ac06e9a2c4a3d35
eop <eop@cs.toronto.edu>
2026-01-05 19:46:35 -0500

    eval 3 hours

--- evaluate_policy.sh
+++ evaluate_policy.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16gb
 #SBATCH --gres=gpu:A100-PCI-80GB:1
-#SBATCH --time=24:00:00
+#SBATCH --time=3:00:00
 #SBATCH --qos=high
 
 export PYTHONPATH="/nas/ucb/eop/Reward-Model-Overoptimization/rlhf/grpo/:/nas/ucb/eop/Reward-Model-Overoptimization/:$PYTHONPATH"
