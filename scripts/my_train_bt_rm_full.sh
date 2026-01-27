ee2650913a7fbe7d7d2088a6080235d317ef0e79
eop <eop@cs.toronto.edu>
2026-01-26 19:59:51 -0500

    5ep_rms

--- scripts/my_train_bt_rm_full.sh
+++ scripts/my_train_bt_rm_full.sh
@@ -83,7 +83,7 @@ fi
 
 learning_rate=2e-5
 max_length=2000
-num_train_epochs=2
+num_train_epochs=5
 gradient_accumulation_steps=16
 per_device_train_batch_size=4
 per_device_eval_batch_size=4
--- scripts/submit_seeds.sh
+++ scripts/submit_seeds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Default values
-START_SEED=${1:-300}
-END_SEED=${2:-309}
+START_SEED=${1:-400}
+END_SEED=${2:-409}
 SAVE_LAST_ONLY=${3:-True}
 SKIP_OPTIMIZER=${4:-True}
 
