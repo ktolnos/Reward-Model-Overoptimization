c2b154772d14fa893f4a62fd41178cb50d0e94d5
eop <eop@cs.toronto.edu>
2026-03-12 13:05:48 -0400

    4B

--- scripts/my_train_bt_rm_full.sh
+++ scripts/my_train_bt_rm_full.sh
@@ -42,8 +42,8 @@ dataset_name=(
 #  "/nas/ucb/eop/Reward-Model-Overoptimization/experimental/data/Qwen3-8B-Embedding-Adv-RM-step_3"
 )
 # base_model='Qwen/Qwen3-4B-Instruct-2507'
-base_model='Qwen/Qwen3-4B-Base'
-# base_model='Qwen/Qwen3-4B'
+# base_model='Qwen/Qwen3-4B-Base'
+base_model='Qwen/Qwen3-4B'
 # base_model='Qwen/Qwen3-4B-Instruct-2507'
 seed=${1:-19}
 save_last_only=${2:-False}
