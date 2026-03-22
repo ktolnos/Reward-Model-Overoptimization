72fbcacaedc0f4cbbc53e30706f36adc1fcaba08
eop <eop@cs.toronto.edu>
2026-03-22 13:09:43 -0400

    8B LoRA policy 4BInstructRM

--- scripts/rlhf/grpo/grpo.sh
+++ scripts/rlhf/grpo/grpo.sh
@@ -261,7 +261,7 @@ CUDA_VISIBLE_DEVICES=${gpu}  accelerate launch  \
     --epsilon_high 0.28 \
     --mask_truncated_completions False \
     --use_vllm True \
-    --vllm_gpu_memory_utilization 0.3 \
+    --vllm_gpu_memory_utilization 0.5 \
     --vllm_mode "colocate" \
     --beta ${beta} \
     --log_completions True \
